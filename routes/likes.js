16149abd34c7e2b50d19f963726763f12d9a1279
Yashwanth <yashwanthssyash@gmail.com>
2025-05-04 07:39:12 +0530

    Removed files as part of reorganization

--- routes/likes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const likesController = require('../controllers/likes_controller');
-
-
-router.get('/toggle', likesController.toggleLike);
-module.exports = router;
